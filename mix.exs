3bc632c7118e10c1f60224c4c8e5b7820779e35f
Patrick Dwyer <patricknevindwyer@gmail.com>
2019-12-08 12:16:36 -0500

    Updated mix for publishing

--- mix.exs
+++ mix.exs
@@ -17,23 +17,21 @@ defmodule Chunky.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
   
   defp package do
      [
          licenses: ["BSD-3-Clause"],
-         files: ~w(lib .formatter.exs mix.exs README* LICENSE*)
+         files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+         links: %{"GitHub" => "https://github.com/patricknevindwyer/chunky"}
      ] 
   end
 end
